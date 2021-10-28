bc36af634e88d4901ee3cc27f7e61fe375314eb7
Gabriela Nogueira <88865948+gabietje@users.noreply.github.com>
2021-10-28 11:01:18 -0300

    multiply
    Simple program to learn new skills on git

---
+++ multiply.c
@@ -0,0 +1,19 @@
+#include <stdio.c>
+#include <stdlib.c>
+
+int main(void)
+{
+    printf("Add a value for y: ");
+
+
+    scanf(%d, &y);
+    printf("Add a value for x: ");
+    scanf(%d, &x);
+    printf("The end result of %d times %d is %d\n", y, x, multiply(y, x));
+    return 0;
+}
+
+int multiply(int y, int x)
+{
+    return (x * y);
+}
