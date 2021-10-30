3703195a778b739425143c64eeacc586d37a9b0b
Gabriela Nogueira <88865948+gabietje@users.noreply.github.com>
2021-10-29 22:43:01 -0300

    multiply program with changes
    hopefully, this time is correct

--- multiply.c
+++ multiply.c
@@ -1,14 +1,16 @@
 #include <stdio.c>
 #include <stdlib.c>
 
+int multiply(int y, int x);
+
 int main(void)
 {
     printf("Add a value for y: ");
-
-
     scanf(%d, &y);
+    
     printf("Add a value for x: ");
     scanf(%d, &x);
+    
     printf("The end result of %d times %d is %d\n", y, x, multiply(y, x));
     return 0;
 }
