66d7f8975866daa8195877f5a67851b42838e8cb
Soham-1616 <sohampatil200616@gmail.com>
2026-07-23 15:17:06 +0530

    Add Binary Search in Java

---
+++ binarySearch/BinarySearch.java
@@ -0,0 +1,54 @@
+// Binary Search in java
+
+package binarySearch;
+
+import java.util.Scanner;
+
+public class BinarySearch {
+
+    public static int binarySearch(int[] arr, int key) {
+        int low = 0;
+        int high = arr.length - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] == key) {
+                return mid;
+            } else if (arr[mid] < key) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the size of the array: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter " + n + " elements in sorted order:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.print("Enter the element to search: ");
+        int key = sc.nextInt();
+
+        int result = binarySearch(arr, key);
+
+        if (result != -1) {
+            System.out.println("Element found at index: " + result);
+        } else {
+            System.out.println("Element not found.");
+        }
+
+        sc.close();
+    }
+}
